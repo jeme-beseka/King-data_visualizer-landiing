dd2f15ddd91c61a05fe052b5925a24bc09f6c64c
Jeme Beseka F <j.beseka@gmail.com>
2026-05-02 23:16:53 +0100

    SEO meta tag

--- src/seo/SEOHead.jsx
+++ src/seo/SEOHead.jsx
@@ -19,6 +19,7 @@ const SEOHead = ({ title, description, url, image, robots }) => {
 
   return (
     <Helmet>
+      <meta name="google-site-verification" content="DbDQzUPULc-YzS3PbGGp95hvb6pEbjU8tsISh3y-hFw" />
       {/* Primary Meta Tags */}
       <title>{finalTitle}</title>
       <meta name="description" content={finalDescription} />
